ab1914188131aa1142a65f9e7a4bdf4d2317d784
810807 <810807@SAIT225654IT.ACDM.DS.SAIT.CA>
2021-01-31 15:38:52 -0700

    New Code Created a new Java Application for HelloWorld.

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -7,7 +7,7 @@ package helloworld;
 
 /**
  *
- * @author annikav
+ * @author Annika Vestre
  */
 public class HelloWorld {
 
@@ -15,7 +15,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello world!");
     }
     
 }
